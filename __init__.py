89483c32d6d44ae2e8d52e98a56f098bc7394276
EthanKuch <46532846+EthanKuch@users.noreply.github.com>
2023-03-14 17:47:56 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,24 +1,39 @@
 from mycroft import MycroftSkill, intent_file_handler
 from mycroft import MycroftSkill, intent_handler
 from adapt.intent import IntentBuilder
+# import removes_context
+from mycroft.skills.context import removes_context
+from mycroft.util import LOG
 
 
 class EasyShopping(MycroftSkill):
+    # Edit in main class: class EasyShopping(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.category_str = ''
+        self.color_str = ''
+        self.brand_str = ''
+        self.kw_str = ''
+        self.img_multi = ''
+        self.img_hand = ''
+        self.log.info(LOGSTR + "_init_ EasyShoppingSkill")
 
     @intent_file_handler('shopping.easy.intent')
     def handle_shopping_easy(self, message):
         self.speak_dialog('shopping.easy')
-    
-#     @intent_handler('view.goods.intent')
-#     def handle_view_goods(self, message):
-#         self.speak('Taking a photo now. Please wait a second for me to get the result.')
-#         self.speak('I find some goods here, you can ask me whatever goods you want.')
 
     @intent_handler('view.goods.intent')
     def handle_view_goods(self, message):
-      self.speak_dialog('take.photo')
+        self.speak_dialog('take.photo')
+        self.img_multi = ''
+        self.img_hand = ''
+
+        # suppose we use camera to take a photo here, 
+        # then the function will return an image path
+        self.img_multi = 'Path_To_Image/multi.jpeg'
+
+        self.speak('I find some goods here, you can ask me whatever goods you want.')
+
 
 
 #     @intent_handler('is.there.any.goods.intent')
@@ -108,7 +123,55 @@ class EasyShopping(MycroftSkill):
     @intent_handler(IntentBuilder('FinishOneItem').require('Finish').build())
     def handle_finish_current_item(self, message):
        self.speak('Got you request. Let\'s continue shopping!')
+    
+    
+    @intent_handler(IntentBuilder('ViewItemInHand').require('ViewItemInHandKeyWord'))
+    def handle_view_item_in_hand(self, message):
+        self.speak_dialog('take.photo')
+        self.img_multi = ''
+        self.img_hand = ''
+    
+        # suppose we use camera to take a photo here, 
+        # then the function will return an image path
+        self.img_hand = 'Path_To_Image/2.jpeg'
+
+        # suppose we call CV API here to get the result, 
+        # the result will all be list, then we use generate_str() to create string
+        self.category_str = generate_str(['milk', 'bottle', 'drink'])
+        self.brand_str = generate_str(['Dutch Lady', 'Lady'])
+        self.color_str = generate_str(['white', 'black', 'blue'])
+        self.kw_str = ' '.join(['milk', 'bottle', 'protein', 'pure', 'farm'])
+
+        # set the context
+        self.set_context('getDetailContext')
+
+        # speak dialog
+        self.speak_dialog('item.category', {'category': self.category_str})
+        
+    @intent_handler(IntentBuilder('AskItemBrand').require('Brand').require('getDetailContext').build())
+    def handle_ask_item_brand(self, message):
+        self.handle_ask_item_detail('brand', self.brand_str)
+    
+    # under class EasyShoppingSkill(MycroftSkill):
+    @intent_handler(IntentBuilder('FinishOneItem').require('Finish').require('getDetailContext').build())
+    @removes_context('getDetailContext')
+    def handle_finish_current_item(self, message):
+        self.speak('Got you request. Let\'s continue shopping!')
+        self.types_str = ''
+        self.color_str = ''
+        self.logo_str = ''
+        self.kw_str = ''
+        self.img_hand = ''
+        self.img_multi = ''
+    
+    @intent_handler(IntentBuilder('NoContext').one_of('Category', 'Color', 'Brand', 'Kw', 'Info'))
+    def handle_no_context2(self, message):
+        self.speak('Please let me have a look at what\'s in your hand first.')
+    
 
 def create_skill():
     return EasyShopping()
+
+
+# Step2: self.img_multi
 
